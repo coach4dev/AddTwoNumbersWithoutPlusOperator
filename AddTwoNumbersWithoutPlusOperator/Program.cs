ee3e1e2e201fb3523d5b51f91d8beee0c830a3fc
coach4dev <info@coach4dev.com>
2019-05-23 07:29:32 -0700

    Add project files.

---
+++ AddTwoNumbersWithoutPlusOperator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AddTwoNumbersWithoutPlusOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The sume of 29 and 33 is: " + Add(29, 33));
+            Console.ReadLine();
+        }
+
+        static int Add(int a, int b)
+        {
+            if (b == 0)
+                return a;
+            int c = a ^ b;
+            int d = (a & b) << 1;
+            return Add(c, d);
+        }
+    }
+}
